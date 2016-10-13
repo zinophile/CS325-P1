46c2f4e617c85f904392d065ca69dbde25125dca
osbaldo-esquivel <osbaldo.esquivel@gmail.com>
2016-10-12 21:30:37 -0700

    Edits

--- main.py
+++ main.py
@@ -7,14 +7,20 @@
   algorithms will determine if there is a max.
   Algorithms are based on examples found at
   https://goo.gl/oYh2NL.
+
+  Team Members:
+  Darin Etherington
+  Osbaldo Esquivel
+  Jared Wasinger
 '''
 
 
 '''
             Algorithm 1: Enumeration
+
   Loop over each pair of indices i,j and
   compute the sum of A[k] from k=i to k=j.
-  Keep the best sum you have found so far
+  Keep the best sum you have found so far.
 '''
 
 def enumAlgo(arr):
@@ -22,20 +28,24 @@ def enumAlgo(arr):
 	sumMax = 0
 	small = 0
 	large = 0
+    idx = 0
 
 	for idx in range(leng)):
 		for temp in range(idx,len(arr)):
-			sumTemp = 0
+			sumTemp = 0  #reset temporary sum
 			for x in arr[idx:temp+1]:
+                #add to sum for this slice
+                #of the array
 				sumTemp = sumTemp + x
 			if sumMax < sumTemp:
-				sumMax = sumTemp
+				sumMax = sumTemp   #new max
 				large = temp
 				small = idx
 	return sumMax, small, large
 
-    '''
-         Algorithm 2: Better Enumeration
+'''
+    Algorithm 2: Better Enumeration (iteration)
+
   Notice that in the previous algorithm the same
   sum is computed many times. In particular, notice
   that the sum of A[k] from k=i to k=j can be
@@ -43,4 +53,21 @@ def enumAlgo(arr):
   k=j-1 in O(1) time, rather than starting from
   scratch. Write a new version of the first
   algorithm that takes advantage of this obversation.
-  '''
+'''
+
+def enumBetterAlgo(arr):
+    sumTemp = 0
+    sumMax = 0
+    small = 0
+    large = 0
+    idx = 0
+
+    for idx in range(len(arr)):
+        sumTemp = 0   #reset sumTemp
+        for temp in range(i,len(arr)):
+            sumTemp += arr[temp]  #running sum
+            if sumMax < sumTemp:
+                sumMax = sumTemp  #new max
+                small = idx
+                large = temp
+        return sumMax, small, large
