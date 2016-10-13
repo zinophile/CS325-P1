d4d9b2d4e516dcf74436146fa4b38af0426fa0cc
osbaldo-esquivel <osbaldo.esquivel@gmail.com>
2016-10-12 21:00:39 -0700

    Start second algorithm

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 #/usr/bin/env python
 
 '''
-                      Project 1                          
-  There are four algorithms that will run for    
-  this program to compare them. These        
-  algorithms will determine if there is a max  
+                      Project 1
+  There are four algorithms that will run for
+  this program to compare them. These
+  algorithms will determine if there is a max.
+  Algorithms are based on examples found at
+  https://goo.gl/oYh2NL.
 '''
 
 
 '''
-            Algorithm 1: Enumeration              
-  Loop over each pair of indices i,j and            
-  compute the sum of A[k] from k = i to j.       
-  Keep the best sum you have found so far   
-'''  
+            Algorithm 1: Enumeration
+  Loop over each pair of indices i,j and
+  compute the sum of A[k] from k=i to k=j.
+  Keep the best sum you have found so far
+'''
 
 def enumAlgo(arr):
 	sumTemp = 0
@@ -31,4 +33,14 @@ def enumAlgo(arr):
 				large = temp
 				small = idx
 	return sumMax, small, large
-	
+
+    '''
+         Algorithm 2: Better Enumeration
+  Notice that in the previous algorithm the same
+  sum is computed many times. In particular, notice
+  that the sum of A[k] from k=i to k=j can be
+  computed from the sum of A[k] from k=i to
+  k=j-1 in O(1) time, rather than starting from
+  scratch. Write a new version of the first
+  algorithm that takes advantage of this obversation.
+  '''
